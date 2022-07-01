0f8d3056ca7aef0070466e662c001b840dde0fa4
lingtangg <60630600+lingtangg@users.noreply.github.com>
2022-07-01 18:06:00 +1000

    Complete custom enumerables project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,67 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index(&block)
+    index = 0
+    my_each do |el|
+      block.call(el, index)
+      index += 1
+    end
+  end
+
+  def my_select(&block)
+    selected = []
+    my_each do |el|
+      selected.push(el) if block.call(el)
+    end
+    selected
+  end
+
+  def my_all?(&block)
+    counter = 0
+    my_each do |el|
+      if block.call(el) then counter += 1 end
+    end
+    counter == length
+  end
+
+  def my_any?(&block)
+    my_each do |el|
+      if block.call(el) then return true end
+    end
+    false
+  end
+
+  def my_none?(&block)
+    my_each do |el|
+      if block.call(el) then return false end
+    end
+    true
+  end
+
+  def my_count(&block)
+    if block_given?
+      count = 0
+      my_select do |el|
+        if block.call(el) then count += 1 end
+      end
+      count
+    else
+      length
+    end
+  end
+
+  def my_map(&block)
+    mapped = []
+    my_each { |el| mapped.push(block.call(el)) }
+    mapped
+  end
+
+  def my_inject(memo = 0, &block)
+    injected = memo
+    my_each do |el|
+      injected = block.call(injected, el)
+    end
+    injected
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +69,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each(&block)
+    i = 0
+    while i < length
+      block.call(self[i])
+      i += 1
+    end
+    self
+  end
 end
